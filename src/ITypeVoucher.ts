251dfefcb2ee7fc077303686ead768eda9def2dd
PhanThanhDong-Crush01 <97138310+PhanThanhDong-Crush01@users.noreply.github.com>
2024-01-23 00:18:35 +0700

    2 create nodejs crud type voucher (#6)
    * feat: crud nodejs typevoucher
    ref: #2
    
    * test: ref:#2
    
    ---------
    
    Co-authored-by: LuuThiHuyenTrang <cupitk102@gmail.com>

---
+++ src/ITypeVoucher.ts
@@ -0,0 +1,4 @@
+export interface ITypeVoucher {
+  _id: string;
+  name: string;
+}
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routers/index.ts";
-
+import CategoryModel from "./models/category.ts";
 dotenv.config();
 const { PORT, DB_URL } = process.env;
 
@@ -14,7 +14,9 @@ app.use(express.json());
 
 const connect = async () => {
   await mongoose
-    .connect(`${DB_URL}`)
+    .connect(
+      `mongodb+srv://phantankk91:phanthanhdong@cluster0.svclp2s.mongodb.net/?retryWrites=true&w=majority`
+    )
     .then(() => console.log("Connected to MongoDB Successfuly!"))
     .catch((error) => console.error(`Error: ${error.message}`));
 };
