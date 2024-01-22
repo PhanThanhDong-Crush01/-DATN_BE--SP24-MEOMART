e94fe7235ca8fd03ea580b828ff7dd18ccc8b725
LuuThiHuyenTrang <cupitk102@gmail.com>
2024-01-23 00:07:41 +0700

    test: ref:#2

--- src/controllers/type_voucher.ts
+++ src/controllers/type_voucher.ts
@@ -98,7 +98,7 @@ export const updateTypeVoucher = async (req, res) => {
         message: "Cập nhật sản phẩm thất bại!",
       });
     }
-
+    ///fygkyihhgj,hjmghgj
     return res.status(200).json({
       message: "Cập nhật sản phẩm thành công!",
       datas: data,
